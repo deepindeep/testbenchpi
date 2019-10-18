d6239146d501e7833c6bf2a768fbc86db6f35385
michaelsizonenko <dethline@te.net.ua>
2019-10-18 15:04:40 +0300

    task 1

--- task1/deactivate_hotspot.sh
+++ task1/deactivate_hotspot.sh
@@ -4,6 +4,12 @@ for line in '/^interface\swlan0$/s/^/#/' '/^\tstatic\sip_address=192.168.4.1\/24
 do
 sed -i $line /etc/dhcpcd.conf
 done
+
+for line in '/^interface=wlan0$/s/^/#/' '/^dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h$/s/^/#/'
+do
+sed -i $line /etc/dnsmasq.conf
+done
+
 systemctl stop dnsmasq
 systemctl stop hostapd
 systemctl disable hostapd
