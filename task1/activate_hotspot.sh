d450b900800d553fb8349980671f5253ff141a8a
michaelsizonenko <dethline@te.net.ua>
2019-10-18 14:46:50 +0300

    task 1

--- task1/activate_hotspot.sh
+++ task1/activate_hotspot.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+for line in '/^#interface\swlan0$/s/^#//' '/^#\tstatic\sip_address=192.168.4.1\/24$/s/^#//' '/^#\tnohook\swpa_supplicant$/s/^#//'
+do
+sed -i $line /etc/dhcpcd.conf
+done
 systemctl enable hostapd
 systemctl start dnsmasq
 systemctl start hostapd
--- task1/deactivate_hotspot.sh
+++ task1/deactivate_hotspot.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+for line in '/^interface\swlan0$/s/^/#/' '/^\tstatic\sip_address=192.168.4.1\/24$/s/^/#/' '/^\tnohook\swpa_supplicant$/s/^/#/'
+do
+sed -i $line /etc/dhcpcd.conf
+done
 systemctl stop dnsmasq
 systemctl stop hostapd
 systemctl disable hostapd
