37838912eaa0a67f6da35a91b05ed848ba7abf28
michaelsizonenko <dethline@te.net.ua>
2019-10-30 12:18:51 +0200

    task4

--- task4/check_data_sheet.py
+++ task4/check_data_sheet.py
@@ -1,16 +1,50 @@
-from utils import build_sheet_service, SheetIDNotFoundException, InvalidDataColumnException, \
-    InvalidInputDataStructure, InvalidParamsException, InvalidURLException
-
+from utils import build_sheet_service, validate_sheet_id
+import pytz
+import datetime
 
 SHEET = "https://docs.google.com/spreadsheets/d/1fsTbXKN-yUrlh4j433ndduyX8xyAdxJ-SlC8XkVw76A/edit#gid=0"
 
-last_row = None
+
+def utc_time_stamp_to_utc_datetime(ts):
+    return datetime.datetime.utcfromtimestamp(int(int(ts) / 1000))
+
+
+def local_time_zone_to_utc_datetime(local_time, time_zone):
+    format_str = '%m/%d/%Y %I:%M:%S %p' if "PM" in local_time or "AM" in local_time else '%m/%d/%Y %H:%M:%S'
+    local = pytz.timezone(time_zone)
+    naive = datetime.datetime.strptime(local_time, format_str)
+    local_dt = local.localize(naive, is_dst=None)
+    utc_dt = local_dt.astimezone(pytz.utc)
+    utc_dt = utc_dt.replace(tzinfo=None)
+    return utc_dt
+
+
+def compare_times(utc_dt, utc_dt2):
+    return datetime.timedelta(minutes=-6) < utc_dt2 - utc_dt < datetime.timedelta(minutes=6)
+
+
+def get_data():
+    service = build_sheet_service()
+    sheet_id = validate_sheet_id(SHEET)
+    result = service.spreadsheets().values().get(
+        spreadsheetId=sheet_id, range="DATA!A:R").execute()
+    data = dict()
+    for i in range(len(result["values"][0])):
+        data[result["values"][0][i].strip()] = result["values"][-1][i]
+    return data
 
 
 def main():
-    global last_row
-    service = build_sheet_service()
+    data = get_data()
+    time_zone = data["TimeZone"]
+    lt = data["Localtime"]
+    utc_ts = data["UTC"]
+    utc_dt = utc_time_stamp_to_utc_datetime(utc_ts)
+    utc_dt2 = local_time_zone_to_utc_datetime(lt, time_zone)
+    check = compare_times(utc_dt, utc_dt2)
+    return data
 
 
 if __name__ == "__main__":
     main()
+    print("Bye!")
--- task4/test_check_data_sheet.py
+++ task4/test_check_data_sheet.py
@@ -1,16 +1,48 @@
 import unittest
 import sys
+import datetime
 import check_data_sheet
 from unittest.mock import patch
 
 
 class TestCheckDataSheet(unittest.TestCase):
 
+    test_header_keys = ['TimeZone', 'Localtime', 'localms', 'UTC ', 'DID', 'RH1', 'W', 'L1', 'L2', 'T1', 'T2', 'RH2', 'B1', 'B2', 'V', 'BB1', 'BB2', 'F']
+
     def test_last_row(self):
         test_args = ["check_data_sheet.py"]
         with patch.object(sys, 'argv', test_args):
-            check_data_sheet.main()
-            self.assertIsNotNone(check_data_sheet.last_row, "Sheet's last row is None!")
+            result = check_data_sheet.main()
+            self.assertIsNotNone(result, "Sheet's last row is None!")
+            self.assertIsInstance(result, dict, "Result data is not an instance of the dict")
+            self.assertTrue(all([i.strip() in result for i in self.test_header_keys]), "Incorrect dict structure")
+            self.assertTrue(any([i is not None for i in result.values()]), "Incorrect dict structure")
+
+    def test_timestamp_convert(self):
+        test_ts = 1558405365495
+        test_result = datetime.datetime(2019, 5, 21, 2, 22, 45)
+        result = check_data_sheet.utc_time_stamp_to_utc_datetime(test_ts)
+        self.assertEqual(test_result, result, "UTC converter datetime objects are not equal")
+
+    def test_localtime_convert(self):
+        test_timezone = "America/Los_Angeles"
+        test_local_time = "5/20/2019 19:22:45"
+        test_result = datetime.datetime(2019, 5, 21, 2, 22, 45)
+        result = check_data_sheet.local_time_zone_to_utc_datetime(test_local_time, test_timezone)
+        self.assertEqual(test_result, result, "Local converter datetime objects are not equal")
+
+    def test_compare_time(self):
+        now = datetime.datetime.utcnow()
+        assert_true = check_data_sheet.compare_times(now, datetime.datetime.utcnow())
+        self.assertTrue(assert_true)
+        assert_false = check_data_sheet.compare_times(now, datetime.datetime.utcnow()+datetime.timedelta(minutes=10))
+        self.assertFalse(assert_false)
+        assert_false2 = check_data_sheet.compare_times(now, datetime.datetime.utcnow()+datetime.timedelta(minutes=-10))
+        self.assertFalse(assert_false2)
+        assert_true2 = check_data_sheet.compare_times(now, datetime.datetime.utcnow()+datetime.timedelta(minutes=5))
+        self.assertTrue(assert_true2)
+        assert_true3 = check_data_sheet.compare_times(now, datetime.datetime.utcnow()+datetime.timedelta(minutes=-5))
+        self.assertTrue(assert_true3)
 
 
 if __name__ == "__main__":
